e8542ae86987f3d855d8be073e5538ea0f51b7bf
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-15 11:02:04 +0400

    console

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // task 1
+            // Console.WriteLine(" text enter :");
+            // string txt=Console.ReadLine();
+            // Console.WriteLine(txt.Replace("  "," "));
+
+            // task 3;
+            // Console.WriteLine(" text enter :");
+            // string txt=Console.ReadLine();
+            // Console.WriteLine(txt.Contains(txt.StartsWith("A"),' '));
+            //Console.WriteLine(txt.StartsWith("A"));
+
+            //task4
+           // Console.WriteLine("imput daxil edin:");
+           // int imput = Convert.ToInt32(Console.ReadLine());
+           // int[] arr = { 20, 12, 23, 21 };
+           //   Array.Resize(ref arr, arr.Length+1);
+           // arr[arr.Length-1] = imput;
+           // foreach (var item in arr)
+          //  {
+           //     Console.WriteLine(item);
+           // }
+           // Console.ReadLine();
+
+
+            //sinifde yazdigimiz task 
+            // static string FindFirstWord(string str)
+            // {
+            //   str = str.Trim();
+
+            //return str.Substring(0, str.IndexOf(' '));
+            // return str.Split(' ')[0];
+        }
+    }
+}
